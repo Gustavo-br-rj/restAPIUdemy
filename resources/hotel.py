a434e0c07826f742ffa2ae54bd4619de5be2bb96
Gustavo-br-rj <73792790+Gustavo-br-rj@users.noreply.github.com>
2021-01-03 13:56:37 -0300

    This commits changes from working with lists in memory to use a simple sqlite database.

--- resources/hotel.py
+++ resources/hotel.py
@@ -27,57 +27,62 @@ hoteis = [
 
 class Hoteis(Resource):
     def get(self):
-        return {'hoteis':hoteis}
+        return {'hoteis':[hotel.json() for hotel in HotelModel.query.all()]}
 
 class Hotel(Resource):
     argumentos = reqparse.RequestParser()
-    argumentos.add_argument('nome')
-    argumentos.add_argument('estrelas')
+    argumentos.add_argument('nome', type=str, required=True, help="The field 'nome' cannot be blank")
+    argumentos.add_argument('estrelas', type=float, required=True, help="The field 'estrelas' cannot be blank")
     argumentos.add_argument('diaria')
     argumentos.add_argument('cidade')
 
-    def find_hotel(hotel_id):
-        for hotel in hoteis:
-            if hotel['hotel_id'] == hotel_id:
-                return hotel
-        return None
-
     def get(self, hotel_id):
-        hotel = Hotel.find_hotel(hotel_id)
+        hotel = HotelModel.find_hotel(hotel_id)
         if hotel: #similar if hotel is not None
-            return hotel
+            return hotel.json()
         return {'message': 'Hotel not found'}, 404 #not found
 
     def post(self, hotel_id):
+        if HotelModel.find_hotel(hotel_id):
+            return {"message": "Hotel id '{}' already exists.".format(hotel_id)}, 400 #Bad request
+
         dados = Hotel.argumentos.parse_args()
-        obj_hotel = HotelModel(hotel_id, **dados)
-        novo_hotel = obj_hotel.json()
+        hotel = HotelModel(hotel_id, **dados)
 
-        hoteis.append(novo_hotel)
+        try:
+            hotel.save_hotel()
+        except:
+            return {'message':'An internal error ocurred trying to save hotel.'}, 500 #Internal server error
 
-        return novo_hotel, 200 #success
+        return hotel.json()
 
     def put(self, hotel_id):
         dados = Hotel.argumentos.parse_args()
 
-        obj_hotel = HotelModel(hotel_id, **dados)
-        novo_hotel = obj_hotel.json()
+        finded_hotel = HotelModel.find_hotel(hotel_id)
 
-        hotel = Hotel.find_hotel(hotel_id)
+        if finded_hotel:
+            finded_hotel.update_hotel(**dados)
+            try:
+                finded_hotel.save_hotel()
+            except:
+                return {'message':'An internal error ocurred trying to save hotel.'}, 500 #Internal server error
+            return finded_hotel.json(), 200 #Success
 
-        if hotel:
-            hotel.update(novo_hotel)
-            return novo_hotel, 200 #Success
+        hotel = HotelModel(hotel_id, **dados)
+        try:
+            hotel.save_hotel()
+        except:
+            return {'message':'An internal error ocurred trying to save hotel.'}, 500 #Internal server error
 
-        hoteis.append(novo_hotel)
-        return novo_hotel, 201 #Created
+        return hotel.json(), 201 #Created
 
     def delete(self, hotel_id):
-        global hoteis
-        size_before = len(hoteis)
-        hoteis = [hotel for hotel in hoteis if hotel['hotel_id'] != hotel_id]
-
-        if len(hoteis) == size_before:
-            return {'message': 'Hotel not found'}, 404 #Not found
-
-        return {'message': 'Hotel deleted.'}, 200 #Success
+        hotel = HotelModel.find_hotel(hotel_id)
+        if hotel:
+            try:
+                hotel.delete_hotel()
+            except:
+                return {'message':'An internal error ocurred trying to delete hotel.'}, 500 #Internal server error
+            return {'message': 'Hotel deleted'}, 200 #Success
+        return {'message': 'Hotel not found.'}, 404 #Not found
