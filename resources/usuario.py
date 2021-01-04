9caeb8bec26bc469873c5ed4df3dc469b51bff95
Gustavo-br-rj <73792790+Gustavo-br-rj@users.noreply.github.com>
2021-01-03 23:46:38 -0300

    Login created with JWT.

--- resources/usuario.py
+++ resources/usuario.py
@@ -1,5 +1,11 @@
 from flask_restful import Resource, reqparse
 from models.usuario import UserModel
+from flask_jwt_extended import create_access_token
+from werkzeug.security import safe_str_cmp
+
+atributos = reqparse.RequestParser()
+atributos.add_argument( 'login', type=str, required=True, help="The field 'login' cannot be left blank.")
+atributos.add_argument( 'password', type=str, required=True, help="The field 'password' cannot be left blank.")
 
 class User(Resource):
 
@@ -22,9 +28,6 @@ class User(Resource):
 
 class UserRegister(Resource):
     def post(self):
-        atributos = reqparse.RequestParser()
-        atributos.add_argument( 'login', type=str, required=True, help="The field 'login' cannot be left blank.")
-        atributos.add_argument( 'password', type=str, required=True, help="The field 'password' cannot be left blank.")
         dados = atributos.parse_args()
 
         if UserModel.find_by_login(dados['login']):
@@ -37,3 +40,15 @@ class UserRegister(Resource):
             return {'message':'User created successfully.'}, 201 #Created
         except:
             return {'message':'An internal error ocurred trying to save user.'}, 500 #Internal server error
+
+class UserLogin(Resource):
+
+    @classmethod
+    def post(cls):
+        dados = atributos.parse_args()
+        user = UserModel.find_by_login(dados['login'])
+
+        if user and safe_str_cmp( user.password, dados['password']):
+            token_acesso = create_access_token(identity=user.user_id)
+            return {'access_token': token_acesso}, 200
+        return { 'message': 'The username or password is incorrect.'}, 401 #Unauthorized
