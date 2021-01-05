786ad445716fb299209566c36e9b0868898e6ba2
Gustavo-br-rj <73792790+Gustavo-br-rj@users.noreply.github.com>
2021-01-05 00:03:45 -0300

    These files were changed to treat login/logout and block access to some functions that depends on login.

--- resources/usuario.py
+++ resources/usuario.py
@@ -1,7 +1,8 @@
 from flask_restful import Resource, reqparse
 from models.usuario import UserModel
-from flask_jwt_extended import create_access_token
+from flask_jwt_extended import create_access_token, jwt_required, get_raw_jwt
 from werkzeug.security import safe_str_cmp
+from blacklist import BLACKLIST
 
 atributos = reqparse.RequestParser()
 atributos.add_argument( 'login', type=str, required=True, help="The field 'login' cannot be left blank.")
@@ -16,6 +17,7 @@ class User(Resource):
             return user.json()
         return {'message': 'User not found'}, 404 #not found
 
+    @jwt_required
     def delete(self, user_id):
         user = UserModel.find_user(user_id)
         if user:
@@ -52,3 +54,12 @@ class UserLogin(Resource):
             token_acesso = create_access_token(identity=user.user_id)
             return {'access_token': token_acesso}, 200
         return { 'message': 'The username or password is incorrect.'}, 401 #Unauthorized
+
+
+class UserLogout(Resource):
+
+    @jwt_required
+    def post(self):
+        jwt_id = get_raw_jwt()['jti'] #JWT Token Identifier
+        BLACKLIST.add(jwt_id)
+        return {'message':'Logged out successfully.'}, 200
