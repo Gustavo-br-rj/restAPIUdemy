d9aaa0a4246ff693f2796987e4b89d7bdb82a078
Gustavo-br-rj <73792790+Gustavo-br-rj@users.noreply.github.com>
2021-01-11 16:55:37 -0300

    Email confirmation

--- resources/usuario.py
+++ resources/usuario.py
@@ -1,12 +1,16 @@
 from flask_restful import Resource, reqparse
+from flask import make_response, render_template
 from models.usuario import UserModel
 from flask_jwt_extended import create_access_token, jwt_required, get_raw_jwt
 from werkzeug.security import safe_str_cmp
 from blacklist import BLACKLIST
+import traceback
 
 atributos = reqparse.RequestParser()
 atributos.add_argument( 'login', type=str, required=True, help="The field 'login' cannot be left blank.")
 atributos.add_argument( 'password', type=str, required=True, help="The field 'password' cannot be left blank.")
+atributos.add_argument( 'email', type=str)
+atributos.add_argument( 'ativado', type=bool)
 
 class User(Resource):
 
@@ -31,17 +35,26 @@ class User(Resource):
 class UserRegister(Resource):
     def post(self):
         dados = atributos.parse_args()
+        if not dados.get('email') or dados.get('email') is None:
+            return {"message":"The field 'email' cannot be left blank."}, 400
+
+        if UserModel.find_by_email(dados['email']):
+            return {"message":"The email '{}' already exists.".format(dados['email'])}, 400
 
         if UserModel.find_by_login(dados['login']):
             return {"message": "The login '{}' already exists.".format(dados['login'])}
 
         user = UserModel(**dados)
+        user.ativado = False
 
         try:
             user.save_user()
-            return {'message':'User created successfully.'}, 201 #Created
+            user.send_confirmation_email()
         except:
+            user.delete_user()
+            traceback.print_exc()
             return {'message':'An internal error ocurred trying to save user.'}, 500 #Internal server error
+        return {'message':'User created successfully.'}, 201 #Created
 
 class UserLogin(Resource):
 
@@ -51,10 +64,11 @@ class UserLogin(Resource):
         user = UserModel.find_by_login(dados['login'])
 
         if user and safe_str_cmp( user.password, dados['password']):
-            token_acesso = create_access_token(identity=user.user_id)
-            return {'access_token': token_acesso}, 200
+            if user.ativado:
+                token_acesso = create_access_token(identity=user.user_id)
+                return {'access_token': token_acesso}, 200
+            return {'message':'User not confirmed.'}, 400
         return { 'message': 'The username or password is incorrect.'}, 401 #Unauthorized
-
 
 class UserLogout(Resource):
 
@@ -63,3 +77,18 @@ class UserLogout(Resource):
         jwt_id = get_raw_jwt()['jti'] #JWT Token Identifier
         BLACKLIST.add(jwt_id)
         return {'message':'Logged out successfully.'}, 200
+
+
+class UserConfirm(Resource):
+
+    @classmethod
+    def get(cls, user_id):
+        user = UserModel.find_user(user_id)
+        if not user:
+            return {"message": "User id '{}' not found.".format(user_id)}, 404
+
+        user.ativado = True
+        user.save_user()
+        # return {"message": "User id '{}' confirmed successfully.".format(user_id)}
+        headers = {'Content-Type':'text/html'}
+        return make_response(render_template('user_confirm.html', email=user.email, usuario=user.login),200,headers)
